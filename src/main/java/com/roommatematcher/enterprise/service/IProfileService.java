d50ed41c8a29b15d1817314ba11548795638edf1
mrmaxmax1423 <maximilianods@ymail.com>
2021-10-10 18:23:15 -0400

    Javadoc for IProfileService
    Added javadoc for fetchBySmokingPreference and fetchByLocation

--- src/main/java/com/roommatematcher/enterprise/service/IProfileService.java
+++ src/main/java/com/roommatematcher/enterprise/service/IProfileService.java
@@ -10,7 +10,17 @@ public interface IProfileService {
      */
     Profile fetchByGender(String gender);
 
+    /**
+     * Fetch a profile with a given smoking preference.
+     * @param smokingPreference a boolean representing if a user smokes or not
+     * @return all profiles matching the smoking preference,  or null if no matches found.
+     */
     Profile fetchBySmokingPreference(Boolean smokingPreference);
 
+    /**
+     * Fetch a profile with a given location.
+     * @param location a string representing where a user is looking for roommates
+     * @return all profiles matching with a matching location,  or null if no matches found.
+     */
     Profile fetchByLocation(String location);
 }
