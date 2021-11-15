6b321ddfe5cbed2a61ae1cc51ad35810392cc0e7
kootinja <kootinja@mail.uc.edu>
2021-11-14 23:43:30 -0500

    added components to the receiving end of the JSON

--- src/main/java/com/roommatematcher/enterprise/RoomMateController.java
+++ src/main/java/com/roommatematcher/enterprise/RoomMateController.java
@@ -1,7 +1,11 @@
 package com.roommatematcher.enterprise;
 
+import com.roommatematcher.enterprise.dto.Location;
 import com.roommatematcher.enterprise.dto.Profile;
+import com.roommatematcher.enterprise.service.ILocationService;
 import com.roommatematcher.enterprise.service.IProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -9,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -18,6 +21,10 @@ public class RoomMateController {
      * Handles the root (/) endpoint and start a start page
      * @return
      **/
+     @Autowired
+     IProfileService profileService;
+     ILocationService locationService;
+
     @RequestMapping("/")
     public String index(){
         return "start";
@@ -25,19 +32,56 @@ public class RoomMateController {
 
     @GetMapping("/profile")
     @ResponseBody
-    public List<Profile> fetchProfiles(){
+    public List<Profile> fetchAllProfiles(){
 
-        return IProfileService.fetchAll();
+        return profileService.fetchAll();
     }
 
     @GetMapping("/profile/{id}/")
-    public ResponseEntity fetchMealItemById(@PathVariable("id") String id) {
-        Profile profiles = (Profile) IProfileService.fetchProfiles(Integer.parseInt(id));
+    public ResponseEntity fetchProfiles(@PathVariable("id") String id) {
+        Profile profiles = profileService.fetchByProfileDescription(Integer.parseInt(id));
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         return new ResponseEntity(profiles, headers, HttpStatus.OK);
     }
 
+    @GetMapping("/location/{id}/")
+    public ResponseEntity fetchLocations(@PathVariable("id") String id) {
+        Location location = locationService.fetchByAddress(String.parseInt(id));
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity(location, headers, HttpStatus.OK);
+    }
+    @PostMapping(value="/location", consumes="application/json", produces="application/json")
+    public Profile createProfiles(@RequestBody Profile profile) throws Exception {
+
+        Profile newProfile = null;
+        try{
+            newProfile = profileService.save(profile);
+        } catch (Exception e) {
+            //TODO ADD LOGGING
+            log.error("profile :" + e);
+
+        }
+        return profile;
+    }
+
+    @DeleteMapping("/profile/{id}/")
+    public ResponseEntity deleteMealItem(@PathVariable("id") String id) {
+        try {
+            profileService.delete(Integer.parseInt(id));
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+
+    }
 
 
 }
+
+
+
+
